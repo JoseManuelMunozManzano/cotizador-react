3931f502d325cd36e5b3240cc7a3703c44ac003e
Neimerc <Neil_Mercury@yahoo.es>
2020-11-28 12:22:01 +0100

    Leyendo la información que el usuario selecciona

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styled from '@emotion/styled';
 
@@ -43,11 +43,28 @@ const Boton = styled.button`
 `;
 
 const Formulario = () => {
+  const [datos, guardarDatos] = useState({
+    marca: '',
+    year: '',
+    plan: 'basico',
+  });
+
+  // Extraer los valores del state
+  const { marca, year, plan } = datos;
+
+  // Leer los datos del formulario y colocarlos en el state
+  const obtenerInformacion = e => {
+    guardarDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
       <Campo>
         <Label>Marca</Label>
-        <Select>
+        <Select name="marca" value={marca} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
@@ -57,7 +74,7 @@ const Formulario = () => {
 
       <Campo>
         <Label>Año</Label>
-        <Select>
+        <Select name="year" value={year} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -74,8 +91,22 @@ const Formulario = () => {
 
       <Campo>
         <Label>Plan</Label>
-        <InputRadio type="radio" name="plan" value="basico" /> Básico
-        <InputRadio type="radio" name="plan" value="completo" /> Completo
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === 'basico'}
+          onChange={obtenerInformacion}
+        />
+        Básico
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === 'completo'}
+          onChange={obtenerInformacion}
+        />
+        Completo
       </Campo>
 
       <Boton type="button">Cotizar</Boton>
